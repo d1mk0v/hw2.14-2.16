6edcf0863949dbae35442373b9522dcbe67dee0f
d1mk0v <79874763588@ya.ru>
2023-07-19 11:27:19 +0500

    hw2.15

--- src/main/java/org/example/SortMethods.java
+++ src/main/java/org/example/SortMethods.java
@@ -15,18 +15,20 @@ public class SortMethods {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(1) + 1_000_000;
         }
-        return arr;
+        int copy[] = Arrays.copyOf(arr, arr.length);
+        return copy;
     }
 
-    public static void sortBubble(int[] arr) {
+    public static void sortBubble(int[] copy) {
+
         generateRandomArray();
 
         long start = System.currentTimeMillis();
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    swapElements(arr, j, j + 1);
+        for (int i = 0; i < copy.length - 1; i++) {
+            for (int j = 0; j < copy.length - 1 - i; j++) {
+                if (copy[j] > copy[j + 1]) {
+                    swapElements(copy, j, j + 1);
                 }
             }
         }
@@ -34,46 +36,46 @@ public class SortMethods {
         System.out.println("bubbleSpeed - " + (System.currentTimeMillis() - start));
     }
 
-    public static void sortSelection(int[] arr) {
+    public static void sortSelection(int[] copy) {
         generateRandomArray();
 
         long start = System.currentTimeMillis();
 
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < copy.length - 1; i++) {
             int minElementIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minElementIndex]) {
+            for (int j = i + 1; j < copy.length; j++) {
+                if (copy[j] < copy[minElementIndex]) {
                     minElementIndex = j;
                 }
             }
-            swapElements(arr, i, minElementIndex);
+            swapElements(copy, i, minElementIndex);
         }
 
         System.out.println("selectionSpeed - " + (System.currentTimeMillis() - start));
 
     }
 
-    public static void sortInsertion(int[] arr) {
+    public static void sortInsertion(int[] copy) {
         generateRandomArray();
 
         long start = System.currentTimeMillis();
 
-        for (int i = 1; i < arr.length; i++) {
-            int temp = arr[i];
+        for (int i = 1; i < copy.length; i++) {
+            int temp = copy[i];
             int j = i;
-            while (j > 0 && arr[j - 1] >= temp) {
-                arr[j] = arr[j - 1];
+            while (j > 0 && copy[j - 1] >= temp) {
+                copy[j] = copy[j - 1];
                 j--;
             }
-            arr[j] = temp;
+            copy[j] = temp;
         }
 
         System.out.println("insertionSpeed - " + (System.currentTimeMillis() - start));
     }
 
-    public static void swapElements(int[] arr, int indexA, int indexB) {
-        int tmp = arr[indexA];
-        arr[indexA] = arr[indexB];
-        arr[indexB] = tmp;
+    public static void swapElements(int[] copy, int indexA, int indexB) {
+        int tmp = copy[indexA];
+        copy[indexA] = copy[indexB];
+        copy[indexB] = tmp;
     }
 }
